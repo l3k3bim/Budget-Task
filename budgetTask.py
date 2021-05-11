d42e6fc7373ea79558f32d27a27b84b65613c7e0
l3k3bim <lekeabim@gmail.com>
2021-05-11 11:48:38 -0700

    redid the budget task

--- budgetTask.py
+++ budgetTask.py
@@ -1,51 +1,47 @@
 class Budget:
 
-  def __init__(self, category, amount): # class attributes
+  def __init__(self, category): # class attributes
     self.category = category
-    self.amount = amount
+    self.amount = 1000
 
 # class methods
-  def deposit(self):
-    depositAmount = float(input('How much would you like to deposit?\n'))
-    self.newBalance = depositAmount + self.amount
-    return self.newBalance
+  def deposit(self, amount):
+    
+    self.amount += amount
+    return self.amount
 
 
-  def withdrawal(self):
-    withdrawAmount = float(input('How much would you like to withdraw?\n'))
-    self.newBalance = self.amount - withdrawAmount
-    return self.newBalance
+  def withdrawal(self, amount):
+    
+    self.amount -= amount
+    return self.amount
 
 
-  def get_balance(self):
-    print('Your current balance is %d' % self.newBalance)
-
-  def transfer(self):
-    transferOptions = int('what do you want to do? 1. option1 2. option2')
-    if transferOptions == 1:
-      return self.deposit()
+  def get_balance(self, amount):
+    if self.amount < amount:
+      return False
     else:
-      return self.withdrawal()
-
-    print(self.newBalance)
-
-#I still don't understand how to do this transfer part      
-
-    
+      return True
     
 
-    
+  def transfer(self, amount):
+    if self.get_balance(amount) == True:
+      self.amount -= amount
+      return "You have transferred from one category to another"
+    else:
+      return "You have insufficient balance"  
+   
 
+category = Budget("Clothing")
 
-food = Budget('Food', 3000)
+print("This is a deposit for clothing", category.deposit(800))
+#print("This is a withdrawal for clothing", category.withdrawal(200))
 
-clothing = Budget('Clothing', 2500)
+category_1 = Budget("food")
+print("This is a deposit for food", category.deposit(1000))
+print(category_1.get_balance(400))
 
-entertainment = Budget('Entertainment', 1000)
+transfer = category_1.transfer(600)
 
-salary = Budget('Salary', 9000)
-
-food.withdrawal()
-food.get_balance()
 
 
